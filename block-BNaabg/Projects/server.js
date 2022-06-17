89f43231ce6c0b676f76d840afffb9c315b33921
vikas2489 <vk55002678@gmail.com>
2022-06-17 17:39:17 +0530

    i just looked out for the POST method in the review video and then solved all other routes own by own

--- block-BNaabg/Projects/server.js
+++ block-BNaabg/Projects/server.js
@@ -1,6 +1,7 @@
 let http = require("http");
 let fs = require("fs");
 // let qs = require("querystring");
+let url = require("url");
 const userDir = __dirname + "/users/";
 
 let server = http.createServer(handleRequest);
@@ -12,32 +13,62 @@ function handleRequest(req, res) {
     })
 
     req.on('end', () => {
+        let parsedUrl = url.parse(req.url, true);
+        // create user
         if (req.method === "POST" && req.url === "/users") {
-            // grab the username from store data
             var username = JSON.parse(store).username;
-            console.log(username);
-            // check whether this username exists in users directory or not
-
-            // We have to create a file using username + append .json to create a proper file
-
-            // wx flag ensures that given username.json should not already exist in users directory, therwise throws an error
-
-            fs.open(userDir + username + ".json", "wx", (err, fd) => {
-                // fd is pointing to newly created file inside users directory
-                // once file is created, we can write content to file
-                // since store has all the data of the user
+            fs.open(userDir + username + ".json", 'wx', (err, fd) => {
+                if (err) return console.log(err);
+                // fd is a file discriptor (an integer) which will points to the newly created file
                 fs.writeFile(fd, store, (err) => {
-                    // err indicated file was not written
-                    // if no error, file was written successfully
-                    // close the file
+                    if (err) return console.log(err);
                     fs.close(fd, (err) => {
-                        // if no err, send response to client
-                        res.end(`${username} successfully created`);
-                    });
-                });
-            });
+                        res.end(`${username} created successfully.`)
+                    })
+                })
+
+            })
+        }
+        // get user
+        else if (parsedUrl.pathname === "/users" && req.method === "GET") {
+            let searchForfileName = parsedUrl.query.username;
+            fs.createReadStream(__dirname + '/users/' + searchForfileName + '.json').pipe(res);
+        }
+        // delete user
+        else if (parsedUrl.pathname === "/users" && req.method === "DELETE") {
+            let searchForfileName = parsedUrl.query.username;
+            fs.unlink(__dirname + '/users/' + searchForfileName + '.json', (err) => {
+                if (err) return console.log(err);
+            })
+        }
+        // update user
+        else if (parsedUrl.pathname === "/users" && req.method === "PUT") {
+            let searchForfileName = parsedUrl.query.username;
+            fs.open(userDir + searchForfileName + ".json", "r+", (err, fd) => {
+                if (err) return console.log(err);
+                fs.ftruncate(fd, (err) => {
+                    if (err) return console.log(err);
+                    fs.writeFile(fd, store, (err) => {
+                        if (err) return console.log(err);
+                        res.end(`${searchForfileName} updated successfully.`)
+                    })
+                })
+            })
+        } else {
+            res.writeHead(404, { "content-type": "text/plain" });
+            res.end("Page Not found 404")
         }
     })
 }
+
+// - create a route to handle update operation
+// - grab the username from querystring
+// - open the file(`fs.open`) using username.json from users directory
+// - use `r+` flag this time, r+ ensures that file already exists
+// - remove the content of file using `fs.ftruncate`
+// - add the updated content using `fs.writeFile`
+// - close the file
+// - send response to client saying `user updated`
+
 
 server.listen(2345);
